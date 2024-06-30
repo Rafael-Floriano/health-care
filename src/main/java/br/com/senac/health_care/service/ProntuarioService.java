aaf6a1e7f92236e87f4317eb6ca1e52d406dd0f4
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-06-30 16:39:00 -0300

    FEAT: implemented new endpoints to get all prontuarios and update specifics prontuarios by id

--- src/main/java/br/com/senac/health_care/service/ProntuarioService.java
+++ src/main/java/br/com/senac/health_care/service/ProntuarioService.java
@@ -1,12 +1,18 @@
 package br.com.senac.health_care.service;
 
 import br.com.senac.health_care.domain.Prontuario;
+import br.com.senac.health_care.domain.RegistroMedico;
 import br.com.senac.health_care.dto.ProntuarioDto;
 import br.com.senac.health_care.repository.ProntuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +20,26 @@ public class ProntuarioService {
 
     private final ProntuarioRepository prontuarioRepository;
 
-    @Transactional(rollbackFor = Exception.class)
     public ResponseEntity<ProntuarioDto> createProntuario(ProntuarioDto prontuarioDto) {
         Prontuario prontuario = prontuarioRepository.save(new Prontuario(prontuarioDto));
         return ResponseEntity.ok(prontuario.toDto());
+    }
+
+    public ResponseEntity<ProntuarioDto> updateProntuario(Long id, ProntuarioDto prontuarioDto) {
+        Prontuario prontuario = prontuarioRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Prontuário não encontrado"));
+
+        prontuario.setDataEntrada(prontuarioDto.getDataEntrada());
+        prontuario.setDataSaida(prontuarioDto.getDataSaida());
+        prontuario.getRegistroMedicos().addAll(prontuarioDto.getRegistroMedicoDtoList().stream().map(RegistroMedico::new).toList());
+        prontuario.setConvenio(prontuario.getConvenio());
+
+        prontuarioRepository.save(prontuario);
+        return ResponseEntity.ok(prontuario.toDto());
+    }
+
+    public ResponseEntity<List<ProntuarioDto>> getAllProntuarios() {
+        List<ProntuarioDto> prontuarios = prontuarioRepository.findAll().stream().map(Prontuario::toDto).toList();
+        return ResponseEntity.ok(prontuarios);
     }
 }
