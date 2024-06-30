ac8791605ce20970b0c15a73769d864ad2f682bb
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-06-30 15:10:36 -0300

    FEAT: implemented RegistroMedico in Prontuario

--- src/main/java/br/com/senac/health_care/domain/Prontuario.java
+++ src/main/java/br/com/senac/health_care/domain/Prontuario.java
@@ -3,14 +3,20 @@ package br.com.senac.health_care.domain;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "prontuario")
 public class Prontuario {
 
     @Id
+    @Column(name = "id_prontuario")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @Column(name = "id_registro_medico")
+    private List<RegistroMedico> registroMedicos;
 
     @Column(name = "data_entrada", nullable = false)
     private LocalDate dataEntrada;
