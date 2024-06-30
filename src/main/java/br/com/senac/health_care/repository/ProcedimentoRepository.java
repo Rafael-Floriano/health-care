12c13f7856f63e1995e2c38b0692489d5e5b2d01
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-06-30 16:58:42 -0300

    FEAT: create repositories for material entity and procedimento entity

---
+++ src/main/java/br/com/senac/health_care/repository/ProcedimentoRepository.java
@@ -0,0 +1,9 @@
+package br.com.senac.health_care.repository;
+
+import br.com.senac.health_care.domain.Procedimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProcedimentoRepository extends JpaRepository<Procedimento, Long> {
+}
