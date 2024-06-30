4a295a391f856d1ee9923f3db17be25b23a71bd8
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-06-30 15:32:19 -0300

    FEAT: created endpoint in RestController to create a new Prontuario

--- src/main/java/br/com/senac/health_care/controller/ProntuarioRestController.java
+++ src/main/java/br/com/senac/health_care/controller/ProntuarioRestController.java
@@ -1,4 +1,24 @@
 package br.com.senac.health_care.controller;
 
+import br.com.senac.health_care.dto.ProntuarioDto;
+import br.com.senac.health_care.service.ProntuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/prontuario")
 public class ProntuarioRestController {
+
+    private final ProntuarioService prontuarioService;
+
+    @PostMapping
+    public ResponseEntity<ProntuarioDto> createProntuario(@RequestBody ProntuarioDto prontuarioDto) {
+        return prontuarioService.createProntuario(prontuarioDto);
+    }
+
 }
--- src/main/java/br/com/senac/health_care/service/ProntuarioService.java
+++ src/main/java/br/com/senac/health_care/service/ProntuarioService.java
@@ -1,4 +1,22 @@
 package br.com.senac.health_care.service;
 
+import br.com.senac.health_care.domain.Prontuario;
+import br.com.senac.health_care.dto.ProntuarioDto;
+import br.com.senac.health_care.repository.ProntuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class ProntuarioService {
+
+    private final ProntuarioRepository prontuarioRepository;
+
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<ProntuarioDto> createProntuario(ProntuarioDto prontuarioDto) {
+        Prontuario prontuario = prontuarioRepository.save(new Prontuario(prontuarioDto));
+        return ResponseEntity.ok(prontuario.toDto());
+    }
 }
