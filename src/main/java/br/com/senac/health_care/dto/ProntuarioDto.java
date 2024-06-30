efe33f9ebc45d92a4f40e43c81308643758ef93b
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-06-30 15:28:39 -0300

    FEAT: implemented 'ProntuarioDto' and 'RegistroMedicoDto'

--- src/main/java/br/com/senac/health_care/dto/ProntuarioDto.java
+++ src/main/java/br/com/senac/health_care/dto/ProntuarioDto.java
@@ -1,4 +1,17 @@
 package br.com.senac.health_care.dto;
 
-public class ProntuarioDto {
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class ProntuarioDto implements Serializable {
+
+    private List<RegistroMedicoDto> registroMedicoDtoList;
+    private LocalDate dataEntrada;
+    private LocalDate dataSaida;
+    private String convenio;
+
 }
--- src/main/java/br/com/senac/health_care/dto/RegistroMedicoDto.java
+++ src/main/java/br/com/senac/health_care/dto/RegistroMedicoDto.java
@@ -1,4 +1,12 @@
 package br.com.senac.health_care.dto;
 
-public class RegistroMedicoDto {
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class RegistroMedicoDto implements Serializable {
+
+    private String conteudo;
+
 }
