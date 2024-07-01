0d22ddffeba208fb9e713406864ae826c70b92ce
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-06-30 22:41:14 -0300

    FEAT: added new query 'findAllByIdPaciente' in AgendamentoRepository

--- src/main/java/br/com/senac/health_care/repository/AgendamentoRepository.java
+++ src/main/java/br/com/senac/health_care/repository/AgendamentoRepository.java
@@ -1,11 +1,22 @@
 package br.com.senac.health_care.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.senac.health_care.domain.Agendamento;
 
+import java.util.List;
+
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
+
+    @Query("""
+    SELECT a FROM Agendamento a
+    WHERE a.paciente.id = :id
+    """)
+    List<Agendamento> findAllByIdPaciente(Long id);
 
 }
